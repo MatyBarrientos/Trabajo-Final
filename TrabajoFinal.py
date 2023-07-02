7fb861d345d7549feccfde5951d1fb2924da18c1
Matías Barrientos <r.m.barrientos@live.com>
2023-07-01 21:06:14 -0300

    1_17_2023
    Modificaciones en General

--- TrabajoFinal.py
+++ TrabajoFinal.py
@@ -1,54 +1,66 @@
 ###Gestion de control. Segunda version.
-from modulo_funciones import obtener_stock,ingreso_producto, busqueda_producto, modificar_producto, listado_productos
-
-        
+from modulo_funciones import obtener_stock,ingreso_producto, busqueda_producto, modificar_producto, listado_productos,impresionGenral,busqueda_nombre
     
 print("""Bienvenido al Sistema de gestión y Control de stock""")
-
-
 
 stock = obtener_stock()
 
 while True:
     print("""Seleccione su Opción.
           1 - Ingresar producto.
-          2 - Buscar producto.
-          3 - Modificar datos del producto.
-          4 - Ver listado de stock.
-          5 - Salir.
+          2 - Buscar producto por código.
+          3 - Buscar producto por nombre.
+          4 - Modificar datos del producto.
+          5 - Ver listado de stock.
+          6 - Salir.
            """)
     while True:
         try:
-            opcion=int(input("Ingrese su opción: "))
+            opcion = int(input("Ingrese su opción: "))
             break
         except:
             print("Tiene que ser un valor númerico: ")
     
-    if opcion==1: 
+    if opcion == 1: 
         print("""\nOpcion 1
 Ingreso de un nuevo producto dentro del stock""")
         ingreso_producto(stock)
      
-    elif opcion==2:   
+    elif opcion == 2:   
         print("""\nOpcion 2
-Buscaremos un artículo en el stock.""")
+Buscaremos un artículo por código en el stock.""")
         busqueda_producto(stock)
+
+    elif opcion == 3:
+        print("""\nOpcion 5
+Buscaremos un artículo por nombre en el stock.""")  
+        busqueda_nombre(stock)
   
-    elif opcion==3:
+    elif opcion == 4:
         print("""\nOpcion 3
 Modificar producto.""")
         modificar_producto(stock)
                
-    elif opcion==4: 
+    elif opcion == 5: 
         print("""\nOpcion 4
 Listado de los productos en el inventario.""")   
         listado_productos(stock)
-            
-    elif opcion==5:
-        print("""\nOpcion 7
+        
+
+          
+    elif opcion == 6:
+        print("""\nOpcion 6
 Saludos.
 Adios!!!!!!!!!!!""")
         break
+    
+
+        
+    elif(opcion==10):
+        impresionGenral(stock)
+        
     else:
         print("""Opcion Incorrecta
 Vuelta al menú""")
+        
+        
--- funciones_generales.py
+++ funciones_generales.py
@@ -1,17 +1,25 @@
-def obtenerEntero(mensaje_entrada,mensaje_error):
+import os
+
+def obtener_entero(mensaje_entrada,mensaje_error):
     while True:
         try:
-            valor=int(input(mensaje_entrada))
+            valor = int(input(mensaje_entrada))
         except:
             print(mensaje_error)
         else:
             return valor
         
-def obtenerFlotante(mensaje_entrada,mensaje_error):
+def obtener_flotante(mensaje_entrada,mensaje_error):
     while True:
         try:
-            valor=float(input(mensaje_entrada))
+            valor = float(input(mensaje_entrada))
         except:
             print(mensaje_error)
         else:
             return valor
+
+def limpiar_pantalla():
+    if os.name == 'posix':  # Para sistemas tipo Unix (Linux, macOS)
+        os.system('clear')
+    elif os.name == 'nt':  # Para sistemas Windows
+        os.system('cls')
